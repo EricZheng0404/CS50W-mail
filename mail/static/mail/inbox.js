738e7ec1223efaa994ef159b10c820e31b3117e1
EricZheng0404 <erryzheng@gmail.com>
2022-11-12 00:41:08 +0800

    first commit

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', new_email);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -31,3 +32,25 @@ function load_mailbox(mailbox) {
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 }
+
+function new_email() {
+  event.preventDefault();
+
+  console.log('Hi')
+
+  // fetch('/emails', {
+  //   method: 'POST',
+  //   body: JSON.stringify({
+  //       recipients: document.querySelector('#compose-recipients').value,
+  //       subject: document.querySelector('#compose-subject').value,
+  //       body: document.querySelector('#compose-body').value
+  //   })
+  // })
+  // .then(response => response.json())
+  // .then(result => {
+  //     // Print result
+  //     console.log(result);
+  //     load_mailbox('sent');
+  // });
+}
+
