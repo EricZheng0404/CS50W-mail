7068db61722b154227318e0b102ee86dc620fac4
EricZheng0404 <erryzheng@gmail.com>
2022-11-14 00:39:48 +0800

    new_email not working

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -16,6 +16,8 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-content-view').style.display = 'none';
+
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -23,34 +25,144 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
+function view_email(id) {
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    document.querySelector('#email-content-view').style.display = 'block';
+
+    document.querySelector('#email-content-view').innerHTML = `
+      <p><strong>From:</strong> ${email.sender}</p>
+      <p><strong>To:</strong>: ${email.recipients}</p>
+      <p><strong>Subject:</strong> ${email.subject}</p>
+      <p><strong>Timestamp:</strong> ${email.timestamp}</p>
+
+      <hr>
+      ${email.body}<br><br>
+    `
+    
+    // If the mail is opened, set to read
+    fetch(`/emails/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          read: true
+      })
+    })
+
+    // Archive the mail
+    const archive_button = document.createElement('button');
+    archive_button.className = "d-grid gap-2 d-md-block"
+    if (email.archived) {
+      archive_button.className = "btn btn-secondary"
+      archive_button.innerHTML = 'Unarchive'
+    } else {
+      archive_button.className = "btn btn-primary"
+      archive_button.innerHTML = 'Archive'
+    }
+
+    archive_button.addEventListener('click', function() {
+      fetch(`/emails/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            archived: !email.archived 
+        })
+      })
+      .then(()=>view_email(id))
+    });
+    document.querySelector('#email-content-view').append(archive_button);
+
+    // Reply button
+    const reply_button = document.createElement('button');
+    reply_button.innerHTML = 'Reply';
+    reply_button.className = "btn btn-success";
+    reply_button.addEventListener('click', function(event) {
+      // Autofill the email info
+      document.querySelector('#compose-recipients').value = email.sender;
+      let subject = email.subject;
+      if (subject.split(" ")[0] != 'Re:') {
+        subject = 'Re: ' + subject;
+      }
+      document.querySelector('#compose-subject').value = subject
+      document.querySelector('#compose-body').value = `
+         On ${email.timestamp} ${email.sender} wrote: ${email.body} <br>
+       `
+      
+       // Send the mail using new_email() function
+      new_email(event);
+
+    });
+    document.querySelector('#email-content-view').append(reply_button);
+  });
+}
+
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-content-view').style.display = 'none';
+
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Get the email API
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    emails.forEach(mail => {
+      
+
+      // Create div for each email
+      const newEmail = document.createElement('div');
+      newEmail.className = 'list-group-item'
+      newEmail.innerHTML = `
+      <p>From: ${mail.sender}</p>
+      <p>Subject: ${mail.subject}</p>
+      <p>Time: ${mail.timestamp}</p>
+      `
+      
+      // Change background color
+      if (mail.read) {
+        // Make it gray
+        newEmail.className = 'list-group-item list-group-item-secondary'
+      } else {
+        // Make it white
+        newEmail.className = 'list-group-item list-group-item-light'
+      }
+      
+      // Add click event listener to the div
+      newEmail.addEventListener('click', function() {
+        view_email(mail.id)
+      })
+      
+      document.querySelector('#emails-view').append(newEmail);
+    });
+  });
 }
 
-function new_email() {
+
+
+function new_email(event) {
   event.preventDefault();
 
-  console.log('Hi')
-
-  // fetch('/emails', {
-  //   method: 'POST',
-  //   body: JSON.stringify({
-  //       recipients: document.querySelector('#compose-recipients').value,
-  //       subject: document.querySelector('#compose-subject').value,
-  //       body: document.querySelector('#compose-body').value
-  //   })
-  // })
-  // .then(response => response.json())
-  // .then(result => {
-  //     // Print result
-  //     console.log(result);
-  //     load_mailbox('sent');
-  // });
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+      // Print result
+      console.log(result);
+      load_mailbox('sent');
+  });
 }
 
